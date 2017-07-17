b03960d4a01fb4d67093dccf7249ee25891b4236
K <1353701931qq.com>
2017-07-17 13:49:20 +0800

    lkk   test

--- src/main/java/com/smartlibrary/controller/testUsergetcontroller.java
+++ src/main/java/com/smartlibrary/controller/testUsergetcontroller.java
@@ -1,9 +1,7 @@
 package com.smartlibrary.controller;
 
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-
+import com.smartlibrary.domain.School;
+import com.smartlibrary.service.testUserService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,8 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.smartlibrary.domain.School;
-import com.smartlibrary.service.testUserService;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 
 @Controller
@@ -27,6 +25,7 @@ public class testUsergetcontroller {
 	//get方法测试
 	public List<School> getschool(HttpServletRequest request) {
 		logger.info("测试开始");
+		System.out.println("245212");
 		System.out.println(request.getQueryString());
 		logger.info("测试结束");
 		return testuserservice.getschool1();

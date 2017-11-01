729c1272129e00a05cb370684d2f5a3603b34a03
hyf <471066902@qq.com>
2017-11-01 19:32:44 +0800

    1

--- src/main/java/com/smartlibrary/controller/bigscreenController.java
+++ src/main/java/com/smartlibrary/controller/bigscreenController.java
@@ -1,12 +1,10 @@
 package com.smartlibrary.controller;
-
 import com.smartlibrary.service.bigScreenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import java.util.List;
 import java.util.Map;
 

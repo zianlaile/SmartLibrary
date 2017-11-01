4fe8e258479915bfb36358e5a97f45a18030f2f6
hyf <471066902@qq.com>
2017-11-01 19:41:34 +0800

    1

---
+++ src/main/java/com/smartlibrary/controller/reportController.java
@@ -0,0 +1,25 @@
+package com.smartlibrary.controller;
+
+import com.smartlibrary.service.reportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by 胡云飞 on 2017/11/1.
+ */
+@Controller
+@RequestMapping("/report")
+public class reportController {
+    @Autowired
+    private reportService reportService;
+    @RequestMapping(method = { RequestMethod.POST },value = "/getreport")
+    @ResponseBody
+    public String getReport(@RequestBody String picBase64Info){
+        picBase64Info = picBase64Info.replaceAll(" ", "+");
+        return reportService.getReport(picBase64Info);
+    }
+}

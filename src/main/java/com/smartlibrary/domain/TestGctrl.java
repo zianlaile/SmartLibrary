d1b12e1d10cc816ef49208849459532063d65935
zianlaile <2542131276@qq.com>
2017-11-16 16:23:14 +0800

    lkk   test

---
+++ src/main/java/com/smartlibrary/domain/TestGctrl.java
@@ -0,0 +1,144 @@
+package com.smartlibrary.domain;
+
+import java.util.Date;
+
+/**
+ * Created by zian on 2017/11/10.
+ */
+public class TestGctrl {
+    private String id;
+    private String account;
+    private String enter_time;
+    private String year;
+    private String month;
+    private String day;
+    private String hour;
+    private String minute;
+    private String second;
+    private String identity;
+    private String school;
+    private String academy;
+    private String major;
+    private String grade;
+    private String gender;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getEnter_time() {
+        return enter_time;
+    }
+
+    public void setEnter_time(String enter_time) {
+        this.enter_time = enter_time;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getHour() {
+        return hour;
+    }
+
+    public void setHour(String hour) {
+        this.hour = hour;
+    }
+
+    public String getMinute() {
+        return minute;
+    }
+
+    public void setMinute(String minute) {
+        this.minute = minute;
+    }
+
+    public String getSecond() {
+        return second;
+    }
+
+    public void setSecond(String second) {
+        this.second = second;
+    }
+
+    public String getIdentity() {
+        return identity;
+    }
+
+    public void setIdentity(String identity) {
+        this.identity = identity;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getAcademy() {
+        return academy;
+    }
+
+    public void setAcademy(String academy) {
+        this.academy = academy;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}

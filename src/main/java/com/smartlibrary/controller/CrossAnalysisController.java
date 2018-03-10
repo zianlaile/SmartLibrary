035f7ee40e32204809c56d9d4f5075d11886dbf0
BachelorXiaoZhang <1551758437@qq.com>
2018-03-10 13:54:31 +0800

    小章

--- src/main/java/com/smartlibrary/controller/CrossAnalysisController.java
+++ src/main/java/com/smartlibrary/controller/CrossAnalysisController.java
@@ -5,24 +5,22 @@ import com.smartlibrary.service.CrossAnalysisService;
 import com.smartlibrary.service.gctrlService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping({ "/cross_analysis" })
 public class CrossAnalysisController {
     private static final Logger logger = Logger.getLogger(gctrlService.class);
     @Autowired
     private CrossAnalysisService crossAnalysisService;
-    @RequestMapping(value = "/getCrossAnalysisByMonth", method = {RequestMethod.GET})
-    @ResponseBody
+
+    @RequestMapping(value = "/getCrossAnalysisByMonth", method = RequestMethod.GET)
     public List<MonthCrossAnalysis> getCrossAnalysisByMonth(){
-        System.out.println(1);
         MonthCrossAnalysis n=new MonthCrossAnalysis();
         return crossAnalysisService.getCrossAnalysisByMonth(n);
     }

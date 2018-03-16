41a963cc888ba393a1fe59b7cb4ea6862f41fc74
aaadfg <244434545@qq.com>
2018-03-16 15:48:00 +0800

    Merge remote-tracking branch 'origin/xinan' into xinan
    # Conflicts:
    #	src/main/webapp/cttp/production/CrossAnalysis.html

--- src/main/java/com/smartlibrary/dao/CrossAnalysisDao.java
+++ src/main/java/com/smartlibrary/dao/CrossAnalysisDao.java
@@ -7,7 +7,7 @@ import com.smartlibrary.domain.CrossAnalysis;
 import java.util.List;
 
 public interface CrossAnalysisDao {
-    List<CrossAnalysis>  CrossAnalysisByDay(CrossAnalysis n);
+     List<CrossAnalysis>CrossAnalysisByDay(CrossAnalysis n);
      List<CrossAnalysis>getCrossAnalysisByMonth(CrossAnalysis n);
      List<CrossAnalysis>getCrossAnalysisByAcademyInUnderGraduate(CrossAnalysis n);
      List<CrossAnalysis>getCrossAnalysisByAcademyInGraduate(CrossAnalysis n);
